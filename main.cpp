1a3bb21988d39b47a7c85d96d14091df5bccbd29
dietmarmenzel <89163030+dietmarmenzel@users.noreply.github.com>
2021-09-28 11:38:44 -0600

    definicion de pines para los botones, potenciometros y pines de la lcd

--- main.cpp
+++ main.cpp
@@ -11,3 +11,19 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 //#include "LCD4bits.h"
+
+//*****************************************************************************
+// Definición de pines
+//*****************************************************************************
+#define d4 14
+#define d5 27
+#define d6 26
+#define d7 25
+#define en 12
+#define rs 13
+
+#define pot1 34
+#define pot2 35
+
+#define boton1 23
+#define boton2 22
